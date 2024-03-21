ba46575d2ae807a0a357d95278ded2efbf8ceeba
libanio-prog <libanio.dev@gmail.com>
2024-03-21 20:34:43 -0300

    criando endpoit html

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,28 @@
 from fastapi import FastAPI
+from fast_zero.schemas import Message
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', status_code=200, response_model=Message)
 def read_root():
     return {'message': 'Olá Mundo!'}
+
+
+@app.get('/ex', response_class=HTMLResponse)
+def read_roots():
+    return """
+    <html>
+    <head>
+        <title>Olá Mundo</title>
+    </head>
+    <body>
+        <h1>Olá Mundo!</h1>
+    </body>
+</html>
+"""
+        
+        
+
+
